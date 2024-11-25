b8ff429d568507cdf932d6728673a287592cb692
Melek Somai <somaimelek@gmail.com>
2024-11-25 15:53:00 -0500

    Adding ESLint

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,50 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:prettier/recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:typescript-sort-keys/recommended", // Enables sorting of object properties
+  ],
+  plugins: [
+    "eslint-plugin-tsdoc",
+    "yaml",
+    "markdown",
+    "import",
+    "typescript-sort-keys",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "tsdoc/syntax": "warn",
+    "sonarjs/no-duplicate-string": "warn",
+    // Importing rules
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "sibling",
+          "parent",
+          "index",
+        ],
+        pathGroups: [
+          {
+            group: "external",
+            pattern: "@inception-health/cdk**",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+      },
+    ],
+  },
+};
