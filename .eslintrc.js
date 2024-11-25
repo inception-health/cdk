78f3fd9ae93d56dbcc316a30522c725936d21804
Melek Somai <somaimelek@gmail.com>
2024-11-25 15:55:13 -0500

    Adding JEST

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-cdk-inception`
+  // which can be found in the workspace. It is at `packages/eslint-config`.
+  extends: ["eslint-config-cdk-inception"],
+};
